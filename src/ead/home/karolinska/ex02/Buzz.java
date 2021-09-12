1c07719fd039d91c1597fddccb342d31c846d3e3
vivianetrindade <trindade.viviane@gmail.com>
2021-09-12 16:56:00 +0200

    added buzz class

---
+++ src/ead/home/karolinska/ex02/Buzz.java
@@ -0,0 +1,25 @@
+package ead.home.karolinska.ex02;
+
+public class Buzz{
+    
+    public static void baffle(String blimp) {
+        System.out.println(blimp);
+        zippo("ping", -5);
+    }
+
+public static void zippo(String quince, int flag) {
+    if (flag < 0) {
+        System.out.println(quince + " zoop");
+    } else {
+        System.out.println("ik");
+        baffle(quince);
+        System.out.println("boo-wa-ha-ha");
+    }
+}
+
+public static void main(String[] args) {
+    
+    zippo("rattle", 13);
+    
+    }
+}
